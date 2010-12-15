1d09bd148fe94c4a8670ac5cf148fb8771d76709
John Fink <jbfink@zamenhof>
2010-12-14 21:28:23 -0500

    Added failing test for link to signup page (l5.28)

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -17,5 +17,9 @@ end
   get '/help'
   response.should have_selector('title', :content => "Help")
  end
+ it "should have a signup page at '/signup'" do
+  get '/signup'
+  response.should have_selector('title', :content => "Sign up")
+end
 
-    end
+   end
