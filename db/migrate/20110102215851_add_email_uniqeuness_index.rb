01afe018a02ce46a8fc576062a4a5bbae9545ffe
John Fink <jbfink@zamenhof>
2011-01-02 17:00:16 -0500

    Added db migration email uniqueness (l6.22)

---
+++ db/migrate/20110102215851_add_email_uniqeuness_index.rb
@@ -0,0 +1,9 @@
+class AddEmailUniqeunessIndex < ActiveRecord::Migration
+  def self.up
+   add_index :users, :email, :unique => true
+  end
+
+  def self.down
+   remote_index :users, :email
+  end
+end
