bbab8710280c3109fedc2888c4d653503f4c89f0
jbfink <jbfink@zamenhof>
2011-01-16 23:10:59 -0500

    Dammit. All that pain just because I couldn't be bothered to read through the whole example. Sigh. Note to self: rake db:migrate, rake db:test:prepare before you bang your head thinking your rspec tests aren't working. Good grief. (l7.4 etc)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -71,13 +71,13 @@ it "should reject long passwords" do
     hash = @attr.merge(:password => long, :password_confirmation => long)
     User.new(hash).should_not be_valid
 end
+end
 describe "password encryption" do
   before(:each) do
     @user = User.create!(@attr)
   end
   it "should have an encrypted password attribute" do
     @user.should respond_to(:encrypted_password)
-end
 end
 end
 end
